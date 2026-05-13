63028dfdd122611cf9ef4ba306b839424fa33a91
park <01043757179a@gmail.com>
2026-05-13 14:56:11 +0900

    feat: add Basic/Advanced toggle to settings screen

--- app/src/main/java/me/magnum/melonds/ui/settings/fragments/MainPreferencesFragment.kt
+++ app/src/main/java/me/magnum/melonds/ui/settings/fragments/MainPreferencesFragment.kt
@@ -3,7 +3,7 @@ package me.magnum.melonds.ui.settings.fragments
 import android.content.Intent
 import android.os.Bundle
 import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import dagger.hilt.android.AndroidEntryPoint
 import me.magnum.melonds.R
 import me.magnum.melonds.ui.about.AboutActivity
@@ -12,10 +12,25 @@ import me.magnum.melonds.ui.settings.PreferenceFragmentTitleProvider
 @AndroidEntryPoint
 class MainPreferencesFragment : BasePreferenceFragment(), PreferenceFragmentTitleProvider {
 
+    private val advancedKeys = listOf(
+        "pref_category_system",
+        "pref_category_video",
+        "pref_category_audio",
+        "pref_category_input",
+        "pref_category_retroachievements",
+        "pref_category_cheats",
+    )
+
     override fun getTitle() = getString(R.string.settings)
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.pref_main, rootKey)
+        applyAdvancedVisibility()
+        findPreference<SwitchPreferenceCompat>("show_advanced_settings")
+            ?.setOnPreferenceChangeListener { _, newValue ->
+                applyAdvancedVisibility(newValue as Boolean)
+                true
+            }
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
@@ -25,6 +40,15 @@ class MainPreferencesFragment : BasePreferenceFragment(), PreferenceFragmentTitl
                 true
             }
             else -> super.onPreferenceTreeClick(preference)
+        }
+    }
+
+    private fun applyAdvancedVisibility(
+        showAdvanced: Boolean = preferenceManager.sharedPreferences
+            ?.getBoolean("show_advanced_settings", false) ?: false
+    ) {
+        advancedKeys.forEach { key ->
+            findPreference<Preference>(key)?.isVisible = showAdvanced
         }
     }
 }
