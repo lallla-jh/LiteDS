d00eb2e68fd8735813c3b24f47d151f485c497e0
park <01043757179a@gmail.com>
2026-06-04 18:14:56 +0900

    build: bump version to 1.0.4 (versionCode 5)

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -4,6 +4,6 @@ object AppConfig {
     const val minSdkVersion = 24
     const val ndkVersion = "28.0.13004108"
 
-    const val versionCode = 4
-    const val versionName = "1.0.3"
+    const val versionCode = 5
+    const val versionName = "1.0.4"
 }
